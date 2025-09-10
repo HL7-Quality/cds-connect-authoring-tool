77285e336082f0afda14fd5b7215eae6e466018f
Taylor Le <taylor@vermonster.com>
2025-09-10 19:52:42 -0400

    Change 'new artifact' button to 'new library'

--- frontend/src/components/artifact/__tests__/ArtifactModal.test.js
+++ frontend/src/components/artifact/__tests__/ArtifactModal.test.js
@@ -74,16 +74,16 @@ describe('<ArtifactModal />', () => {
   });
 
   describe('modal title', () => {
-    it('displays Create New Artifact for new artifacts', async () => {
+    it('displays Create New Library for new artifacts', async () => {
       renderComponent();
 
-      expect(await screen.findByText('Create New Artifact')).toBeInTheDocument();
+      expect(await screen.findByText('Create New Library')).toBeInTheDocument();
     });
 
     it('displays Edit Artifact Details when editing an artifact', async () => {
       renderComponent({ artifactEditing: artifactMock });
 
-      expect(await screen.findByText('Edit Artifact Details')).toBeInTheDocument();
+      expect(await screen.findByText('Edit Library Details')).toBeInTheDocument();
     });
   });
 
